60f34a35c063658151cc394069d840d773991ebb
Grishin Ilya <98251201+WithSoull@users.noreply.github.com>
2025-09-09 15:58:00 +0300

    Make unit tests (#9)
    * add minimock install in Makefile
    
    * add generated mocks and draft test
    
    * make some test for handler and repo layer
    
    ---------
    
    Co-authored-by: Илья Гришин <withsoull@yadnex.ru>

--- go.mod
+++ go.mod
@@ -1,25 +1,29 @@
 module github.com/WithSoull/AuthService
 
-go 1.23.4
+go 1.24.0
+
+toolchain go1.24.1
 
 require (
 	github.com/jackc/pgx/v5 v5.7.5
 	github.com/joho/godotenv v1.5.1
-	golang.org/x/crypto v0.39.0
-	google.golang.org/grpc v1.73.0
-	google.golang.org/protobuf v1.36.6
+	golang.org/x/crypto v0.41.0
+	google.golang.org/grpc v1.75.0
+	google.golang.org/protobuf v1.36.8
 )
 
 require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
-	golang.org/x/sync v0.15.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
 )
 
 require (
 	github.com/Masterminds/squirrel v1.5.4 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/georgysavva/scany v1.2.3 // indirect
+	github.com/gojuno/minimock/v3 v3.4.6 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.8.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -30,9 +34,11 @@ require (
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/net v0.41.0 // indirect
-	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/text v0.26.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/net v0.43.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
+	golang.org/x/text v0.28.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250603155806-513f23925822 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250826171959-ef028d996bc1 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
 )
---
+++ internal/handler/user/tests/update_password_test.go
@@ -0,0 +1,90 @@
+package tests
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	userHandler "github.com/WithSoull/AuthService/internal/handler/user"
+	"github.com/WithSoull/AuthService/internal/service"
+	serviceMocks "github.com/WithSoull/AuthService/internal/service/mocks"
+	desc "github.com/WithSoull/AuthService/pkg/user/v1"
+	"github.com/brianvoe/gofakeit"
+	"github.com/gojuno/minimock/v3"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func TestUpdatePassword(t *testing.T) {
+	type userServiceMockFunc func(mc *minimock.Controller) service.UserService
+
+	type args struct {
+		req *desc.UpdatePasswordRequest
+	}
+
+	var (
+		ctx = context.Background()
+		mc  = minimock.NewController(t)
+
+		id = gofakeit.Int64()
+
+		password = gofakeit.Password(true, true, true, true, true, 8)
+
+		req = &desc.UpdatePasswordRequest{
+			Id:              id,
+			Password:        password,
+			PasswordConfirm: password,
+		}
+
+		serviceErr = errors.New("service error")
+
+		res = &emptypb.Empty{}
+	)
+
+	tests := []struct {
+		name            string
+		args            args
+		want            *emptypb.Empty
+		err             error
+		userServiceMock userServiceMockFunc
+	}{
+		{
+			name: "success case",
+			args: args{
+				req: req,
+			},
+			want: res,
+			err:  nil,
+			userServiceMock: func(mc *minimock.Controller) service.UserService {
+				mock := serviceMocks.NewUserServiceMock(mc)
+				mock.UpdatePasswordMock.Expect(minimock.AnyContext, id, password, password).Return(nil)
+				return mock
+			},
+		},
+		{
+			name: "service error case",
+			args: args{
+				req: req,
+			},
+			want: &emptypb.Empty{},
+			err:  serviceErr,
+			userServiceMock: func(mc *minimock.Controller) service.UserService {
+				mock := serviceMocks.NewUserServiceMock(mc)
+				mock.UpdatePasswordMock.Expect(minimock.AnyContext, id, password, password).Return(serviceErr)
+				return mock
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			userServiceMock := tt.userServiceMock(mc)
+			handler := userHandler.NewHandler(userServiceMock)
+
+			res, err := handler.UpdatePassword(ctx, tt.args.req)
+			require.Equal(t, tt.err, err)
+			require.Equal(t, tt.want, res)
+		})
+	}
+}
