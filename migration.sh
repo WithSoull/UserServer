cd25869452265c443248915d7710b545f77b5bc7
WithSoull <withsoull@yandex.ru>
2025-10-12 01:15:02 +0300

    chore(docker): add Postgres healthcheck; gate services via depends_on; remove migrator sleep

--- migration.sh
+++ migration.sh
@@ -14,8 +14,5 @@ MIGRATION_DIR=${MIGRATION_DIR:-migrations}
 
 MIGRATION_DSN="host=${DB_HOST} port=${DB_PORT} dbname=${DB_NAME} user=${DB_USER} password=${DB_PASSWORD} sslmode=disable"
 
-echo "Waiting for database to start..."
-sleep 2
-
 echo "Running migrations with DSN: ${MIGRATION_DSN}"
 goose -dir "${MIGRATION_DIR}" postgres "${MIGRATION_DSN}" up -v
